45329d6ffaf490a7bf26b9fa01ec7b8e505f7113
Arzadun <sebastianarzadun@gmail.com>
2020-08-29 12:04:12 -0300

    Corrección tarea3.js

--- tareas/clase-5/tarea3.js
+++ tareas/clase-5/tarea3.js
@@ -10,11 +10,8 @@
 document.querySelector("#enviar").onclick = function (event) {
     const cantidadClases = Number(document.querySelector("#cantidad").value);
 
-    for (let i = 0; i < cantidadClases; i++) {
-        crearCampos();
-    }
-
-    crearBotonCalcular();
+    crearClases(cantidadClases);
+    crearBotones();
     ocultarFormulario();
 
     document.querySelector('#calcular').onclick = function () {
@@ -26,11 +23,10 @@ document.querySelector("#enviar").onclick = function (event) {
     document.querySelector('#limpiar').onclick = function () {
         document.querySelector('strong').style.display = 'none';
     }
-
     return false;
 };
 
-function crearBotonCalcular() {
+function crearBotones() {
     const $form = document.querySelector("#clases");
     const boton = document.createElement("button");
     const limpiar = document.createElement("button");
@@ -42,34 +38,35 @@ function crearBotonCalcular() {
     limpiar.type = "reset";
     limpiar.textContent = "Limpiar";
 
-
     $form.appendChild(boton);
     $form.appendChild(limpiar);
 }
 
-function crearCampos() {
+function crearClases(clases) {
     const $form = document.querySelector("#clases");
-    const div = document.createElement("div");
 
-    const inputHoras = document.createElement("input");
-    inputHoras.className = "horas";
-    inputHoras.placeholder = "horas";
-    inputHoras.type = "number";
-    div.appendChild(inputHoras);
+    for (let i = 0; i < clases; i++) {
+        const div = document.createElement("div");
+        const inputHoras = document.createElement("input");
+        inputHoras.className = "horas";
+        inputHoras.placeholder = "horas";
+        inputHoras.type = "number";
+        div.appendChild(inputHoras);
 
-    const inputMinutos = document.createElement("input");
-    inputMinutos.className = "minutos";
-    inputMinutos.placeholder = "minutos";
-    inputMinutos.type = "number";
-    div.appendChild(inputMinutos);
+        const inputMinutos = document.createElement("input");
+        inputMinutos.className = "minutos";
+        inputMinutos.placeholder = "minutos";
+        inputMinutos.type = "number";
+        div.appendChild(inputMinutos);
 
-    const inputSegundos = document.createElement("input");
-    inputSegundos.className = "segundos";
-    inputSegundos.placeholder = "segundos";
-    inputSegundos.type = "number";
-    div.appendChild(inputSegundos);
+        const inputSegundos = document.createElement("input");
+        inputSegundos.className = "segundos";
+        inputSegundos.placeholder = "segundos";
+        inputSegundos.type = "number";
+        div.appendChild(inputSegundos);
 
-    $form.appendChild(div);
+        $form.appendChild(div);
+    }
 }
 
 function ocultarFormulario() {
@@ -79,7 +76,6 @@ function ocultarFormulario() {
     }
 }
 
-
 function calcularTiempo() {
     let acumuladorHoras = 0, acumuladorMinutos = 0, acumuladorSegundos = 0;
 
@@ -87,52 +83,39 @@ function calcularTiempo() {
     const minutos = document.querySelectorAll('.minutos');
     const segundos = document.querySelectorAll('.segundos');
 
-    for (let i = 0; i < horas.length; i++) {
-        let cantidadHoras = Number(horas[i].value);
-        acumuladorHoras += cantidadHoras;
-
-
+    for (let k = 0; k < segundos.length; k++) {
+        let cantidadSegundos = Number(segundos[k].value);
+        acumuladorSegundos += cantidadSegundos;
     }
 
     for (let j = 0; j < minutos.length; j++) {
         let cantidadMinutos = Number(minutos[j].value);
         acumuladorMinutos += cantidadMinutos;
-
-
     }
 
-    for (let k = 0; k < segundos.length; k++) {
-        let cantidadSegundos = Number(segundos[k].value);
-        acumuladorSegundos += cantidadSegundos;
-
+    for (let i = 0; i < horas.length; i++) {
+        let cantidadHoras = Number(horas[i].value);
+        acumuladorHoras += cantidadHoras;
     }
+
 
     calculos(acumuladorHoras, acumuladorMinutos, acumuladorSegundos);
-
 }
 
 function calculos(horas, minutos, segundos) {
 
-    let minutos_A_Horas = 0, restoMinutos = 0;
-    if (minutos <= 60) {
-        restoMinutos = minutos;
-        minutos_A_Horas = 0;
-    } else {
-        minutos_A_Horas = Math.round(minutos / 60);
-        restoMinutos = minutos % 60;
-    }
+    let segundosAMinutos = 0, restoSegundos = 0;
 
-    let segundos_A_Minutos = 0, restoSegundos = 0;
-    if (segundos <= 60) {
-        restoSegundos = segundos;
-        segundos_A_Minutos = 0;
-    } else {
-        segundos_A_Minutos = Math.round(segundos / 60);
-        restoSegundos = segundos % 60;
-    }
+    segundosAMinutos = Math.round(segundos / 60);
+    restoSegundos = segundos % 60;
 
-    horas += minutos_A_Horas;
-    restoMinutos += segundos_A_Minutos;
+    minutos += segundosAMinutos;
+
+    let minutosAHoras = 0, restoMinutos = 0;
+    minutosAHoras = Math.round(minutos / 60);
+    restoMinutos = minutos % 60;
+
+    horas += minutosAHoras;
 
 
     function mostrarResultado() {
@@ -140,7 +123,6 @@ function calculos(horas, minutos, segundos) {
         mostrar.innerText = `Las clases tienen una duracion de ${horas} horas, ${restoMinutos} minutos, y ${restoSegundos} segundos .`
     }
     mostrarResultado();
-
 }
 
 
